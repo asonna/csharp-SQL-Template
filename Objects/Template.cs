93a70962b919a9899f9e1dd46c0942db857f659d
Annie Sonna and Lina Shadrach <annie+lina@>
2016-12-05 13:35:40 -0800

    update to include logic for gathering and storing data in a database

--- Objects/Template.cs
+++ Objects/Template.cs
@@ -1,16 +1,64 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace TEMPLATE.Objects
 {
-  public class TEMPLATE
+  public class TEMPLATE_OBJECT
   {
-    public int TEMPLATEproperty {get; set;}
+    // where TEMPLATE_OBJECTId references a property of the object
+    public int TEMPLATE_OBJECTId {get; set;}
+    public string TEMPLATE_OBJECTDescription {get; set;}
     private List<string> TEMPLATE = new List<string> {};
+    //where TEMPLATE_OBJECT references the Object
 
-    public TEMPLATE(int TEMPLATEuserdata)
+    public TEMPLATE_OBJECT(int TEMPLATEuserdata)
     {
       this.TEMPLATEproperty = TEMPLATEuserdata;
+    }
+
+    public override bool Equals(System.Object TEMPLATE_OTHER_OBJECT)
+    {
+      if (!(otherTask is Task))
+      {
+        return false;
+      }
+      else
+      {
+        TEMPLATE_OBJECT newTEMPLATE_OBJECT = (TEMPLATE_OBJECT) TEMPLATE_OTHER_OBJECT;
+        bool TEMPLATEpropertyEquality = (this.TEMPLATEproperty() == newTEMPLATE_OBJECT.TEMPLATEproperty());
+        return (TEMPLATEpropertyEquality);
+      }
+    }
+
+    public static List<Task> GetAll()
+    {
+      List<TEMPLATE_OBJECT> TEMPLATE_OBJECT_LIST = new List<TEMPLATE_OBJECT>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM TEMPLATE_OBJECTS;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int TEMPLATE_OBJECTId = rdr.GetInt32(0);
+        string TEMPLATE_OBJECTDescription = rdr.GetString(1);
+        TEMPLATE_OBJECT newTEMPLATE_OBJECT = new TEMPLATE_OBJECT(TEMPLATE_OBJECTDescription, taskId);
+        TEMPLATE_OBJECT_LIST.Add(newTEMPLATE_OBJECT);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allTasks;
     }
   }
 }
--- Tests/TestTemplate.cs
+++ Tests/TestTemplate.cs
@@ -2,11 +2,18 @@ using Xunit;
 using System;
 using System.Collections.Generic;
 using TEMPLATE.Objects;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace  TEMPLATE
 {
-  public class TEMPLATE
+  public class TEMPLATETest : IDisposable
   {
+    public TEMPLATETest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=todo_test;Integrated Security=SSPI;";
+    }
+
     [Fact]
     public void TEMPLATE_true()
     {
@@ -15,5 +22,11 @@ namespace  TEMPLATE
       //Assert
       Assert.Equal(true/false, TEMPLATE);
     }
+
+    public void Dispose()
+    {
+      TEMPLATE_OBJECT.DeleteAll(); //TEMPLATE_OBJECT references the object name
+    }
+
   }
 }
